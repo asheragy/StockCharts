eb016c18b25c5a69fb4ff59516d3723f22ea33ee
andy <andy@cerion.org>
2020-08-22 16:22:33 -0700

    added basic instrumentation tests

--- app/src/androidTest/java/org/cerion/stockcharts/ui/charts/ChartsFragmentTest.kt
+++ app/src/androidTest/java/org/cerion/stockcharts/ui/charts/ChartsFragmentTest.kt
@@ -1,7 +1,17 @@
 package org.cerion.stockcharts.ui.charts
 
+import android.widget.FrameLayout
+import androidx.appcompat.view.menu.ActionMenuItem
+import androidx.core.view.get
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.mikephil.charting.charts.LineChart
 import kotlinx.coroutines.test.runBlockingTest
 import org.cerion.stockcharts.R
 import org.cerion.stockcharts.repository.PreferenceRepository
@@ -11,6 +21,7 @@ import org.cerion.stockcharts.ui.charts.fakes.FakePriceListRepository
 import org.cerion.stocks.core.repository.CachedPriceListRepository
 import org.cerion.stocks.core.repository.IPriceListRepository
 import org.cerion.stocks.core.web.PriceHistoryDataSource
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -36,9 +47,47 @@ class ChartsFragmentTest : KoinTest {
     }
 
     @Test
-    fun chartsFragment_basicRun() = runBlockingTest {
-        launchFragmentInContainer<ChartsFragment>(null, R.style.AppTheme)
+    fun chartsFragment_defaults() = runBlockingTest {
+        val scenario = launchFragmentInContainer<ChartsFragment>(null, R.style.AppTheme)
 
-        Thread.sleep(5000)
+        // 2 charts by default
+        scenario.onFragment {
+            val rv = it.view?.findViewById<RecyclerView>(R.id.recycler_view)!!
+            Assert.assertEquals(2, rv.adapter!!.itemCount)
+        }
     }
+
+    @Test
+    fun chartsFragment_addOverlay() = runBlockingTest {
+        val scenario = launchFragmentInContainer<ChartsFragment>(null, R.style.AppTheme)
+
+        // Click first item -> add new overlay -> save
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.add_overlay)).perform(click())
+        onView(withId(R.id.save)).perform(click())
+
+        scenario.onFragment {
+            val rv = it.view?.findViewById<RecyclerView>(R.id.recycler_view)!!
+            val frame = rv[0].findViewById<FrameLayout>(R.id.chart_frame)
+            val chart = frame[0] as LineChart
+            Assert.assertEquals(2, chart.data.dataSetCount)
+        }
+    }
+
+    @Test
+    fun chartsFragment_addChart() = runBlockingTest {
+        val scenario = launchFragmentInContainer<ChartsFragment>(null, R.style.AppTheme)
+
+        scenario.onFragment {
+            val menuItem = ActionMenuItem(it.context, 0, R.id.add_price, 0, 0, null)
+            it.onOptionsItemSelected(menuItem)
+        }
+
+        // 3 after adding
+        scenario.onFragment {
+            val rv = it.view?.findViewById<RecyclerView>(R.id.recycler_view)!!
+            Assert.assertEquals(3, rv.adapter!!.itemCount)
+        }
+    }
+
 }
